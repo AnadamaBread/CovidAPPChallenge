8e09feeebf9fd34ac08f2fcd1e49c7aea4cdfee6
AnadamaBread <lbaez133@gmail.com>
2020-08-08 00:23:53 -0400

    EOD Aug 7th

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -54,6 +54,10 @@ const routes: Routes = [
   {
     path: 'account',
     loadChildren: () => import('./pages/account/account.module').then( m => m.AccountPageModule)
+  },
+  {
+    path: 'routine',
+    loadChildren: () => import('./pages/routine/routine.module').then( m => m.RoutinePageModule)
   }
 ];
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBRpsbHvgndaqtkn9KiD7cXFq2SK_lDzsU",
+    authDomain: "covid19-appchallenge.firebaseapp.com",
+    databaseURL: "https://covid19-appchallenge.firebaseio.com",
+    projectId: "covid19-appchallenge",
+    storageBucket: "covid19-appchallenge.appspot.com",
+    messagingSenderId: "728871485189",
+    appId: "1:728871485189:web:4ac13c8d4d2bd7b0f2416c",
+    measurementId: "G-J9W4JN3KJP"
+  }
+
 };
