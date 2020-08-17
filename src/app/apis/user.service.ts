77355d9a7f496c38f792795a338b827a9df5d0a8
AnadamaBread <lbaez133@gmail.com>
2020-08-17 19:06:10 -0400

    First Track medium progress dump

--- src/app/apis/user.service.ts
+++ src/app/apis/user.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from '@angular/fire/firestore';
+// import { Storage } from '@ionic/storage';
+
+import * as firebase from 'firebase';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
+
+import { Router } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { Router, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-signup',
@@ -7,12 +10,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  db=firebase.firestore();
+
+  user={
+    email:"",
+    password:""
+  };
+  firstName = "";
+  lastName = "";
+  birthDay = "";
+
+  constructor(private router:Router) { 
+
+  }
 
   ngOnInit() {
   }
 
   doSignup(){
+    console.log("Creating user with "+this.user.email + " and password"+this.user.password);
+    var email = this.user.email;
+    var pass = this.user.password;
+    var self = this;
+
+    firebase.auth().createUserWithEmailAndPassword(email,pass).catch(
+      function(error){
+        console.log("INSIDE ERROR");
+        console.log(error);
+        var errorCode = error.code;
+	  			var errorMessage = error.message;
+	  			console.log(error.message);
+	  			if(errorCode.length > 0){
+	  				console.log("Failed");
+	  			}
+	  			else{
+	  				console.log("signup ok")
+	  			}
+      }
+    ).then(function(result){
+      var user = firebase.auth().currentUser;
+      var db = firebase.firestore();
+      db.collection("users").add({
+        'uid':user.uid,
+        'firstName': self.firstName,
+        'lastName' : self.lastName,
+        'birthDay' : self.birthDay
+      }).then(function(docRef) {
+        console.log("user written with ID: ", docRef.id);
+
+      }).catch(function(error){
+        console.log("error creating sign up doc");
+      });
+
+      console.log("ACCOUNT SUCCESSFULLY CREATED");
+      console.log(user.uid);
+      firebase.auth().signInWithEmailAndPassword(email,pass).catch(function(error){
+        //catches errors from login method
+			let errorCode = error.code
+			let errorMessage = error.message
+			console.log("Error code: ", errorCode)
+			console.log("Error message: ", errorMessage)
+			//pushes alerts to user based on error
+			if(errorCode === "auth/wrong-password") {
+			  alert("Wrong password.")
+			} else if(errorCode === "auth/user-not-found") {
+			  alert("User does not exist or wrong email.")
+			}
+			console.log(error)
+      }).then(function(result){
+        let user = firebase.auth().currentUser;
+        self.db.collection("users").where("uid", "==", user.uid).get().then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+          console.log(doc.id, "=>", doc.data())
+          let firstName = doc.data().firstName
+          console.log("First Name:", firstName)
+          
+          })
+        }).catch(function(error) {
+          console.log("Error getting documents: ", error)
+          alert("Login failed, try again.")
+        })
+        console.log("Login successful")
+        alert("Login successful, please fill out next page.")
+        self.router.navigate(['/adduserdetails'])
+        })
+
+      })
+
     
   }
 
