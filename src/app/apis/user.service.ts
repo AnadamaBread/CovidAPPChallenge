16744a2e2c75deee9624bb0447d818bf337ad9a2
AnadamaBread <lbaez133@gmail.com>
2020-08-25 15:08:44 -0400

    Service work create and async

--- src/app/apis/user.service.ts
+++ src/app/apis/user.service.ts
@@ -20,6 +20,8 @@ export class UserService {
   users:Array<any> = [];
   current_user: any = null;
 
+  usertype = "patient";
+
   publishEvent(data:any){
     this.eventSub.next(data);
   }
@@ -31,13 +33,61 @@ export class UserService {
 
   constructor( public router:Router) { }
 
+
+  createInteract(date, time, location, topic, words, tone, feel, pros, edit) {
+    var self = this;
+    var iid = null;
+    if(firebase.auth().currentUser != null &&  this.usertype =="patient"){
+      iid = firebase.auth().currentUser.uid;
+      var dab = firebase.firestore();
+      dab.collection("interactions").add({
+        'date': date,
+        'time': time,
+        'location': location,
+        'topic': topic,
+        'words': words,
+        'tone': tone,
+        'feel': feel,
+        'pros':pros,
+        'edit':edit,
+        'linkid': iid
+      }).then(function(docRef){
+        console.log("Document written with ID :", docRef.id );
+      }).catch(function(error){
+        console.error("Error adding document to dab: ", error);
+      });
+    }
+    else{
+      console.log("User not logged in");
+    }
+
+  }
+  
   getInteractions(): any {
     var self = this;
     if(firebase.auth().currentUser != null){
       console.log("Getting user info");
-      
+      let curr_uid = firebase.auth().currentUser.uid;
+      self.users.forEach(function(u_val){
+        if(u_val.uid == curr_uid){
+          self.current_user = u_val;
+        }
+      })
+      self.publishEvent({})
+      console.log("Current user Set in Service");
+    }
+    if(this.current_user != null && this.interactions != null){
+      console.log("Setting User Info and Loading Interactions");
+      // let user_ints = {
+      // }
     }
   }
+
+  // async refresh() {
+  //   let self = this;
+  //   await self.db.collection("interactions")
+  // }
+
 
 
 }
